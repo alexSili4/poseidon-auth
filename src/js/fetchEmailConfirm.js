8b851a300d10b88bc8eb5661977c04dde62aa27c
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-08-15 10:54:13 +0300

    add profile-form

--- src/js/fetchEmailConfirm.js
+++ src/js/fetchEmailConfirm.js
@@ -15,30 +15,47 @@ const fetchEmailConfirm = async (data) => {
   };
 
   try {
-    // const response = await fetch(url, options);
+    const response = await fetch(url, options);
 
-    // if (!response.ok) {
-    //   const error = await response.json();
-    //   const errorMessage = error[0].message;
+    if (!response.ok) {
+      let errorMessage = '';
+      const errors = await response.json();
+      const emailErrorMessage = errors.find(({ field }) => field === 'email')?.message;
+      const passwordErrorMessage = errors.find(({ field }) => field === 'password')?.message;
+      if (emailErrorMessage && passwordErrorMessage) {
+        errorMessage = `${emailErrorMessage}\n${passwordErrorMessage}`;
+      } else {
+        errorMessage = emailErrorMessage || passwordErrorMessage;
+      }
+      throw new Error(errorMessage);
+    }
 
-    //   throw new Error(errorMessage);
-    // }
-
-    // const data = await response.json();
-    console.log(data);
+    const data = await response.json();
     refs.signUpFormEmailInputWrap.classList.add(constants.checkClassName);
-    const { password, ...signUpFormEmailData } = data.SUP2;
+    const { password, ...signUpFormEmailData } = data;
     localStorage.save({ key: localStorageKeys.signUpFormEmail, value: signUpFormEmailData });
 
     setTimeout(() => {
       showSignUpFormProfile();
     }, 1000);
   } catch (error) {
-    console.log(error);
-    refs.signUpFormEmailInputError.textContent = error.message;
-    refs.signUpFormInputPassError.textContent = error.message;
-    refs.signUpFormEmailInputWrap.classList.add(constants.invalidClassName);
-    refs.signUpFormInputPassWrap.classList.add(constants.invalidClassName);
+    const isDoubleError = error.message.includes('\n');
+    const isEmailError = error.message.toLowerCase().includes('email');
+    const isPasswordError = error.message.toLowerCase().includes('пароль');
+
+    if (isDoubleError) {
+      const errors = error.message.split('\n');
+      refs.signUpFormEmailInputError.textContent = errors[0];
+      refs.signUpFormInputPassError.textContent = errors[1];
+      refs.signUpFormEmailInputWrap.classList.add(constants.invalidClassName);
+      refs.signUpFormInputPassWrap.classList.add(constants.invalidClassName);
+    } else if (isEmailError) {
+      refs.signUpFormEmailInputError.textContent = error.message;
+      refs.signUpFormEmailInputWrap.classList.add(constants.invalidClassName);
+    } else if (isPasswordError) {
+      refs.signUpFormInputPassError.textContent = error.message;
+      refs.signUpFormInputPassWrap.classList.add(constants.invalidClassName);
+    }
   }
 };
 
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import FullReload from 'vite-plugin-full-reload';
 export default defineConfig({
   root: 'src',
   build: {
-    // minify: false,
+    minify: false,
     rollupOptions: {
       input: glob.sync('./src/*.html'),
       output: {
